aaae48447c3085907d3f444aedcbe107aaeabffa
derekgp <derek@ntiertraining.com>
2017-09-15 10:26:50 -0700

    Updating client from Github

--- src/com/dgp/Client.java
+++ src/com/dgp/Client.java
@@ -3,7 +3,7 @@ package com.dgp;
 public class Client {
 
 	public static void main(String[] args) {
-		 System.out.println("HOLA MUNDO GATO   999");
+		 System.out.println("HOLA MUNDO GATO   999 PIPER");
 
 	}
 
